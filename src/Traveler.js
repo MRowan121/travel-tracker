e8f36e7e23370f1211c866a98d995e54bc460ce4
Matt Rowan <matt.rowan121@gmail.com>
2023-01-10 17:56:13 -0500

    Add class structure and method for get first name

---
+++ src/Traveler.js
@@ -0,0 +1,13 @@
+class Traveler {
+    constructor(travelerInfo) {
+        this.id = travelerInfo.id;
+        this.name = travelerInfo.name;
+        this.travelerType = travelerInfo.travelerType;
+    };
+
+    getFirstName() {
+        return this.name.split(" ")[0];
+    };
+};
+
+module.exports = Traveler;
