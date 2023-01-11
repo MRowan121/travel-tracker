c1b62bc10619d8528865142b0a46e14982697e49
Matt Rowan <matt.rowan121@gmail.com>
2023-01-10 21:38:56 -0500

    Add destination id and travelers properties

--- src/Trips.js
+++ src/Trips.js
@@ -1,7 +1,9 @@
 class Trips {
     constructor(tripDetails) {
         this.id = tripDetails.id;
-        this.userID = tripDetails.userID
+        this.userID = tripDetails.userID;
+        this.destinationID = tripDetails.destinationID;
+        this.travelers = tripDetails.travelers;
     }
 };
 
