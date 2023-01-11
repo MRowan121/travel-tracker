4f693d914ae4109e672c5b04ede93150c0955a93
Matt Rowan <matt.rowan121@gmail.com>
2023-01-11 10:03:03 -0500

    Add estimated lodging and flight properties

--- src/Destinations.js
+++ src/Destinations.js
@@ -2,6 +2,8 @@ class Destinations {
     constructor(destinationDetails) {
         this.id = destinationDetails.id;
         this.destination = destinationDetails.destination;
+        this.estimatedLodgingCostPerDay = destinationDetails.estimatedLodgingCostPerDay;
+        this.estimatedFlightCostPerPerson = destinationDetails.estimatedFlightCostPerPerson;
     };
 };
 
