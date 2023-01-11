0e0e058c1ef7c361a140a56e81b973f6ab1f83a4
Matt Rowan <matt.rowan121@gmail.com>
2023-01-11 09:28:55 -0500

    Implement dayjs functionality for date property

--- src/Trips.js
+++ src/Trips.js
@@ -1,10 +1,13 @@
+import dayjs from 'dayjs'
+
 class Trips {
     constructor(tripDetails) {
         this.id = tripDetails.id;
         this.userID = tripDetails.userID;
         this.destinationID = tripDetails.destinationID;
         this.travelers = tripDetails.travelers;
+        this.date = dayjs(tripDetails.date);
     }
 };
 
-module.exports = Trips;
+export default Trips;
