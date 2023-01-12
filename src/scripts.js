56fe09ed3e7bde41acc025ec10887de8d7013e98
Matt Rowan <matt.rowan121@gmail.com>
2023-01-11 20:34:02 -0500

    Add global variables and fetch all data function

--- src/scripts.js
+++ src/scripts.js
@@ -6,6 +6,21 @@ import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
-
-
 console.log('This is the JavaScript entry file - your code begins here.');
+
+
+import apiCalls from "./apiCalls";
+
+// Global Variables
+
+let travelerData;
+let tripData;
+let destinationData;
+
+// Functions
+
+apiCalls.fetchAllData().then((data) => {
+    travelerData = data[0].travelerData;
+    tripData = data[1].tripData;
+    destinationData = data[2].destinationData;
+  });
